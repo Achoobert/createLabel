5b371e5829999e1b999b9d342dc581b4fec06716
Isaac Schubert <greenace14@gmail.com>
2023-02-16 17:07:09 +0700

    [add] index.js

---
+++ index.js
@@ -0,0 +1,28 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+try {
+  const labelName = core.getInput('label-name');
+  const labelColor = core.getInput('label-color');
+  const octokit = new github.GitHub(process.env.GITHUB_TOKEN);
+  octokit.issues.listLabelsForRepo({
+    owner: github.context.repo.owner,
+    repo: github.context.repo.repo
+  }).then(({ data }) => {
+    const label = data.find(l => l.name === labelName);
+    if (!label) {
+      octokit.issues.createLabel({
+        owner: github.context.repo.owner,
+        repo: github.context.repo.repo,
+        name: labelName,
+        color: labelColor
+      }).then((response) => {
+        console.log(`Label ${response.data.name} created successfully`);
+      });
+    } else {
+      console.log(`Label ${labelName} already exists`);
+    }
+  });
+} catch (error) {
+  core.setFailed(error.message);
+}
